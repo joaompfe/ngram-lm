487ba25892559dbbca750d01418fd8a7e6d4cc87
joaompfe <joaofe2000@gmail.com>
2021-10-25 17:10:39 +0100

    c: Refactor trie_test.cc

--- src/c/trie_test.cc
+++ src/c/trie_test.cc
@@ -2,49 +2,64 @@
 
 extern "C" {
 #include <unistd.h>
-#include <stdio.h>
 #include "c/trie.h"
 #include "c/ngram.h"
 #include "c/arpa.h"
 }
 
 #include <gtest/gtest.h>
+#include <fstream>
 
-TEST(Trie, TrieNumberOfNgrams)
-{
-    struct trie *trie = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
-    EXPECT_EQ(trie->order, 3);
-    EXPECT_EQ(trie->n_ngrams[0], 209);
-    EXPECT_EQ(trie->n_ngrams[1], 323);
-    EXPECT_EQ(trie->n_ngrams[2], 325);
-}
+const char *TEST_DATA = "./data/tmp.arpa";
 
-TEST(Trie, VocabLookupArray)
+static void validate_trie(const struct trie *t)
 {
-    struct trie *trie = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
-    for (int i = 1; i < trie->n_ngrams[0]; i++) {
+    EXPECT_EQ(t->order, 3);
+
+    EXPECT_EQ(t->n_ngrams[0], 209);
+    EXPECT_EQ(t->n_ngrams[1], 323);
+    EXPECT_EQ(t->n_ngrams[2], 325);
+
+    for (int i = 1; i < t->n_ngrams[0]; i++) {
         EXPECT_TRUE(
-                trie->vocab_lookup[i - 1].hash < trie->vocab_lookup[i].hash);
+                t->vocab_lookup[i - 1].hash < t->vocab_lookup[i].hash);
     }
+
+    EXPECT_EQ(t->arrays[0]->len, 210);
+    EXPECT_EQ(t->arrays[1]->len, 324);
+    EXPECT_EQ(t->arrays[2]->len, 325);
 }
 
-TEST(Trie, QueryNgram)
+TEST(Trie, trie_new_from_arpa)
 {
-    struct trie *trie = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
+    validate_trie(t);
+    trie_delete(t);
+}
+
+TEST(Trie, trie_new_from_arpa_path)
+{
+    struct trie *t = trie_new_from_arpa_path(3, TEST_DATA);
+    validate_trie(t);
+    trie_delete(t);
+}
+
+TEST(Trie, trie_query_ngram)
+{
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
     char const *grams[10];
     grams[0] = "caso";
     grams[1] = "português";
     int n = 2;
-    struct ngram *ngram = trie_query_ngram(trie, (char const **) grams, &n);
+    struct ngram *ngram = trie_query_ngram(t, (char const **) grams, &n);
     EXPECT_EQ(-0.29952f, ngram->probability);
     EXPECT_STREQ(ngram->word->text, "português");
 
-    const struct word *w = &trie->vocab_lookup[trie->n_ngrams[0] - 1];
     grams[0] = "garanta";
     grams[1] = "essa";
     grams[2] = "circulação";
     n = 3;
-    ngram = trie_query_ngram(trie, (char const **) grams, &n);
+    ngram = trie_query_ngram(t, (char const **) grams, &n);
     EXPECT_STREQ(ngram->word->text, "circulação");
     EXPECT_TRUE(ngram->context != nullptr);
     EXPECT_TRUE(ngram->context->context != nullptr);
@@ -52,64 +67,80 @@ TEST(Trie, QueryNgram)
     grams[0] = "havia";
     grams[1] = "é";
     n = 2;
-    ngram = trie_query_ngram(trie, (char const **) grams, &n);
+    ngram = trie_query_ngram(t, (char const **) grams, &n);
     EXPECT_STREQ(ngram->word->text, "é");
     EXPECT_TRUE(ngram->context == nullptr);
+
+    trie_delete(t);
 }
 
-TEST(Trie, LoadAndSaveFromFile)
+const char *OUT_PATH = "./data/tmp.bin";
+
+TEST(Trie, trie_fwrite_and_trie_fread)
 {
-    const char *arpa_path = "./data/tmp.arpa";
-    const char *out_path = "./data/tmp.bin";
-    FILE *f = fopen(out_path, "r");
-    if (f != nullptr) {
-        std::remove(out_path);
-        fclose(f);
-    }
-    struct trie *t = trie_new_from_arpa(3, arpa_open(arpa_path));
-    f = fopen(out_path, "wb");
-    if (f == nullptr) {
-        fprintf(stderr, "Error while opening %s: %s.\n", out_path,
-                strerror(errno));
-        FAIL();
-    }
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
+    FILE *f = fopen(OUT_PATH, "wb");
+    ASSERT_TRUE(f != nullptr) << "Error while opening " << OUT_PATH;
+
     trie_fwrite(t, f);
+    trie_delete(t);
     fclose(f);
-    f = fopen(out_path, "rb");
+    f = fopen(OUT_PATH, "rb");
     ASSERT_TRUE(f != nullptr);
     fclose(f);
 
-    struct trie *trie;
-    f = fopen(out_path, "rb");
-    size_t read = trie_fread(&trie, f);
+    f = fopen(OUT_PATH, "rb");
+    size_t read = trie_fread(&t, f);
     EXPECT_EQ(read, 1);
     fclose(f);
-    EXPECT_EQ(trie->order, 3);
-    EXPECT_EQ(trie->n_ngrams[0], 209);
-    EXPECT_EQ(trie->n_ngrams[1], 323);
-    EXPECT_EQ(trie->n_ngrams[2], 325);
-    char const *grams[2];
-    grams[0] = "caso";
-    grams[1] = "português";
-    int n = 2;
-    struct ngram *ngram = trie_query_ngram(trie, (char const **) grams, &n);
-    EXPECT_EQ(-0.29952f, ngram->probability);
-    EXPECT_STREQ(ngram->word->text, "português");
-    std::remove(out_path);
+    validate_trie(t);
+    trie_delete(t);
+
+    std::remove(OUT_PATH);
 }
 
-TEST(Trie, GetWordText)
+TEST(Trie, trie_save_and_trie_load)
 {
-    struct trie *trie = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
+    trie_save(t, OUT_PATH);
+    FILE *f = fopen(OUT_PATH, "rb");
+    ASSERT_TRUE(f != nullptr);
+    fclose(f);
+    trie_delete(t);
+
+    trie_load(OUT_PATH, &t);
+    validate_trie(t);
+    trie_delete(t);
+
+    std::remove(OUT_PATH);
+}
+
+TEST(Trie, trie_get_word_id_from_text)
+{
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
+
+    EXPECT_EQ(trie_get_word_id_from_text(t, "»"), 0);
+    EXPECT_EQ(trie_get_word_id_from_text(t, "amarrar"), 1);
+    EXPECT_EQ(trie_get_word_id_from_text(t, "afinal"), 200);
+
+    trie_delete(t);
+}
+
+TEST(Trie, trie_word_textncpy)
+{
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
     char word[48];
-    EXPECT_STREQ("»", trie_word_textncpy(trie, 0, word, 48));
-    EXPECT_STREQ("amarrar", trie_word_textncpy(trie, 1, word, 48));
-    EXPECT_STREQ("afinal", trie_word_textncpy(trie, 200, word, 48));
+
+    EXPECT_STREQ("»", trie_word_textncpy(t, 0, word, 48));
+    EXPECT_STREQ("amarrar", trie_word_textncpy(t, 1, word, 48));
+    EXPECT_STREQ("afinal", trie_word_textncpy(t, 200, word, 48));
+
+    trie_delete(t);
 }
 
-TEST(Trie, GetNwp)
+TEST(Trie, trie_get_nwp)
 {
-    struct trie *t = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
     const char *words[] = { "que", "é" };
     struct word *nwp = trie_get_nwp(t, words, 1);
     ASSERT_STREQ(nwp->text, "os");
@@ -125,25 +156,34 @@ TEST(Trie, GetNwp)
     nwp = trie_get_nwp(t, words, 0);
     ASSERT_STREQ(nwp->text, "Para");
 
-    words[0] = "giaonewnfwpofnjANOFawdWAQ";
+    words[0] = "anonexistingword";
     nwp = trie_get_nwp(t, words, 1);
     ASSERT_STREQ(nwp->text, "Para");
 
     words[0] = "Para";
-    words[1] = "giaonewnfwpofnjANOFawdWAQ";
+    words[1] = "anonexistingword";
     nwp = trie_get_nwp(t, words, 2);
     ASSERT_STREQ(nwp->text, "Para");
+
+    words[0] = "Para";
+    words[1] = "é";
+    nwp = trie_get_nwp(t, words, 2);
+    ASSERT_STREQ(nwp->text, "que");
 
     words[0] = "havia";
     words[1] = "é";
     nwp = trie_get_nwp(t, words, 2);
     ASSERT_STREQ(nwp->text, "que");
+
+    trie_delete(t);
 }
 
-TEST(Trie, GetKNwp)
+TEST(Trie, trie_get_k_nwp)
 {
-    struct trie *t = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
-    const char *words[] = { "é", "que" };
+    struct trie *t = trie_new_from_arpa(3, arpa_open(TEST_DATA));
+    const char *words[5];
+    words[0] = "é";
+    words[1] = "que";
     struct word *word_preds[10];
     int k = 10;
     trie_get_k_nwp(t, words, 2, k, word_preds);
@@ -157,42 +197,15 @@ TEST(Trie, GetKNwp)
     ASSERT_STREQ(word_preds[7]->text, "reforça");
     ASSERT_STREQ(word_preds[8]->text, "Na");
     ASSERT_STREQ(word_preds[9]->text, "Em");
-}
 
-TEST(Trie, LoadAndSave)
-{
-    const char *arpa_path = "./data/tmp.arpa";
-    const char *out_path = "./data/tmp.bin";
-    FILE *f = fopen(out_path, "r");
-    if (f != nullptr) {
-        std::remove(out_path);
-        fclose(f);
-    }
-    struct trie *t = trie_new_from_arpa(3, arpa_open(arpa_path));
-    trie_save(t, out_path);
-    f = fopen(out_path, "rb");
-    ASSERT_TRUE(f != nullptr);
+    words[0] = "é";
+    words[1] = "que";
+    words[2] = "que";
+    EXPECT_EXIT(
+            trie_get_k_nwp(t, words, 3, k, word_preds);,
+            ::testing::KilledBySignal(11),
+            ".*"
+    );
+
     trie_delete(t);
-    trie_load(out_path, &t);
-
-    EXPECT_EQ(t->order, 3);
-    EXPECT_EQ(t->n_ngrams[0], 209);
-    EXPECT_EQ(t->n_ngrams[1], 323);
-    EXPECT_EQ(t->n_ngrams[2], 325);
-}
-
-TEST(Trie, NgramProbability)
-{
-    struct trie *t = trie_new_from_arpa(3, arpa_open("./data/tmp.arpa"));
-    char const *words[10];
-    words[0] = "qualquer";
-    words[1] = "ligação";
-    float prob = trie_ngram_probability(t, words, 2);
-    EXPECT_EQ(prob, -0.5990452f);
-
-    words[0] = "uma";
-    words[1] = "maior";
-    words[2] = "aprofundamento";
-    prob = trie_ngram_probability(t, words, 3);
-    EXPECT_EQ(prob, -0.29952f - 0.30103f);
 }
